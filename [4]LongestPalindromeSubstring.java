28822a51c258e691e8807ff75f9696d6ee19c16a
Bingjie Xiao <bxiao0801@gmail.com>
2016-04-07 12:56:02 -0400

    Update [4]LongestPalindromeSubstring.java

--- [4]LongestPalindromeSubstring.java
+++ [4]LongestPalindromeSubstring.java
@@ -5,23 +5,24 @@ public class Solution{
 		if(s==null||s.length()==0){
 			return s;
 		}
-
+		
 		for(int i=0;i<s.length();i++){
 			findLongest(s,i,i);
 			findLongest(s,i,i+1);
 		}
+		
 		return s.substring(low,high+1);
 	}
-
+	
 	public void findLongest(String s,int i,int j){
 		while(i>-1&&j<s.length()&&s.charAt(i)==s.charAt(j)){
 			i--;
 			j++;
 		}
+		
 		if(j-i-1>high-low){
 			low=i+1;
 			high=j-1;
 		}
-
 	}
 }
