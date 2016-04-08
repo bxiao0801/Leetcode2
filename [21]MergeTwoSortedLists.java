a3c0d00eca32a709eeaf9ed4e6728f23ae7cb5d1
Bingjie Xiao <bxiao0801@gmail.com>
2016-04-08 11:40:52 -0400

    Create [21]MergeTwoSortedLists.java

---
+++ [21]MergeTwoSortedLists.java
@@ -0,0 +1,27 @@
+/**
+*recursively merge two lists
+**/
+class ListNode{
+	int val;
+	ListNode next;
+	public ListNode(int x){
+		this.val=x;
+	}
+}
+public class Solution{
+	public ListNode merge(ListNode l1,ListNode l2){
+		if(l1==null){
+			return l2;
+		}
+		if(l2==null){
+			return l1;
+		}
+		if(l1.val<l2.val){
+			l1.next=merge(l1.next,l2);
+			return l1;
+		}else{
+			l2.next=merge(l1,l2.next);
+			return l2;
+		}
+	}
+}
